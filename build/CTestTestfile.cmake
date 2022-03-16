11b718974394bb4604661010910dc43e661453ea
alser3217 <raschetnov.alexei@gmail.com>
2022-03-16 20:25:57 +0300

    list_table

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: K:/Labs/2Year/Polynomials
-# Build directory: K:/Labs/2Year/Polynomials/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("tests")
-subdirs("sample")
